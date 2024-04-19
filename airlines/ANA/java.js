90512bb36586813fce8dc67f3a9e611d34de0d14
Daniel <daniel@Daniel-PC>
2024-04-19 15:39:47 -0700

    Second commit

---
+++ airlines/ANA/java.js
@@ -0,0 +1,23 @@
+// const observer = new IntersectionObserver (entry => {
+//     entries.forEach(entry =>  {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('');
+//         }
+
+//     });
+    
+// });
+
+// observer.observe(document.querySelector('.info')
+
+function Skytrax_Desc() {
+    document.getElementById("Desc").innerHTML = "Skytrax World Airline Awards, described as “the Oscars of the aviation industry”, are most coveted Quality accolades for airlines, and represent a global benchmark of excellence.<br><br>All Nippon Airways (ANA), Japan's largest airline, earned the prestigious 5-Star quality of service designation from SKYTRAX, a globally leading independent international air transport rating organization. ANA is the only Japanese airline to maintain the 5-Star rating for 11 consecutive years since 2013, a testament to its unwavering commitment, dedication and hospitality exhibited by its employees on a daily basis"
+}
+
+function StarAlliance_Desc() {
+    document.getElementById("Desc").innerHTML = "Star Alliance was founded on May 14, 1997 as the first global airline alliance. It linked five major airlines — United Airlines, Scandinavian Airlines, Thai Airways International, Air Canada, and Lufthansa — to create a single network and offered new services to international travelers.<br><br>ANA has been a member of Star Alliance since 1999 and has joint venture partnerships with United Airlines, Lufthansa German Airlines, Swiss International Airlines and Austrian Airlines"
+}
+
+function Cirium_Desc() {
+    document.getElementById("Desc").innerHTML = "All Nippon Airways (ANA), Japan's largest and 5-Star airline for ten consecutive years, has been recognized by travel data and analytics firm Cirium with being ranked first in both Global and Asia Pacific for its exceptional service and reliability in providing on-time performance in 2022."
+}
---
+++ airlines/JAL/java.js
@@ -0,0 +1,11 @@
+function Skytrax_Desc() {
+    document.getElementById("Desc").innerHTML = "Traveling in JAL’s economy class is a pleasant experience because every aspect of the cabin experience has been carefully considered and refined. From our award-winning seats to our collaboratively-designed menus, our economy class inflight experience is bound to impress even the most discerning travelers.<br>Traveling in JAL’s economy class is a pleasant experience because every aspect of the cabin experience has been carefully considered and refined. From our award-winning seats to our collaboratively-designed menus, our economy class inflight experience is bound to impress even the most discerning travelers."
+}
+
+function OneWorld() {
+    document.getElementById("Desc").innerHTML = "Oneworld is a global airline alliance consisting of 13 member airlines. It was founded on 1 February 1999. The alliance's stated objective is to be the first choice airline alliance for the world's frequent international travellers.<br>Japan Airlines has been a member of the oneworld Alliance since 2007. This oneworld member airline flies to some 80 destinations in more than 20 countries. "
+}
+
+function Apex() {
+    document.getElementById("Desc").innerHTML ="The aviation industry gathered Wednesday night to celebrate the airlines and suppliers whose dedication and innovation bore significant advancement in passenger experience over the last year. The prestigious APEX/IFSA Awards Ceremony, hosted by APEX (Airline Passenger Experience Association) and the IFSA (International Flight Services Association), is considered the industry’s premier awards event of the year. <br>Japan Airlines has recieved this award for the third consecutive year, making it the only Japanese airline to be recognized."
+}
